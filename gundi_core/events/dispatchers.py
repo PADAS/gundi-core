6ed57c8874533705839cd2e6e54df30325df50c0
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2025-01-21 09:28:02 -0300

    Improve dispatcher event models to store more info on errors

--- gundi_core/events/dispatchers.py
+++ gundi_core/events/dispatchers.py
@@ -1,5 +1,47 @@
+from typing import Optional
+from pydantic import BaseModel, Field
 from gundi_core.schemas.v2 import DispatchedObservation, UpdatedObservation, CustomDispatcherLog
 from .core import SystemEventBaseModel
+
+
+class DispatcherErrorDetails(BaseModel):
+    error: str = Field(
+        "",
+        title="Error",
+        description="A human-readable string describing the error.",
+    )
+    error_traceback: Optional[str] = Field(
+        "",
+        title="Error Traceback",
+        description="A string with the traceback of the error.",
+    )
+    server_response_status: Optional[int] = Field(
+        None,
+        title="Server Response Status",
+        description="The status code of the server response.",
+    )
+    server_response_body: Optional[str] = Field(
+        "",
+        title="Server Response",
+        description="The response from the server as text.",
+    )
+
+
+class DeliveryErrorDetails(DispatcherErrorDetails):
+    observation: Optional[DispatchedObservation] = Field(
+        None,
+        title="Observation",
+        description="The observation that caused the error.",
+    )
+
+
+class UpdateErrorDetails(DispatcherErrorDetails):
+    observation: Optional[UpdatedObservation] = Field(
+        None,
+        title="Observation",
+        description="The observation that caused the error.",
+    )
+
 
 # Events emmited by dispatchers
 
@@ -8,7 +50,8 @@ class ObservationDelivered(SystemEventBaseModel):
 
 
 class ObservationDeliveryFailed(SystemEventBaseModel):
-    payload: DispatchedObservation
+    schema_version: str = Field("v2", const=True)
+    payload: DeliveryErrorDetails
 
 
 class ObservationUpdated(SystemEventBaseModel):
@@ -16,7 +59,8 @@ class ObservationUpdated(SystemEventBaseModel):
 
 
 class ObservationUpdateFailed(SystemEventBaseModel):
-    payload: UpdatedObservation
+    schema_version: str = Field("v2", const=True)
+    payload: UpdateErrorDetails
 
 
 class DispatcherCustomLog(SystemEventBaseModel):
